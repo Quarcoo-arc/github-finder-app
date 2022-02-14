e888799e2ad15f8285319a7ba1c8f2d7ae917655
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-14 13:12:25 +0000

    NotFound page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,22 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="mx-6">
+      <h1 className="text-5xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-1xl font-light">
+        A React app to search GitHub profiles and see profile details. This
+        project is was done by
+        <a href="https://github.com/Quarcoo-arc/github-finder-app"> Minoq 😇</a>
+        , also known as
+        <strong>
+          <a href="https://github.com/Quarcoo-arc"> Michael Quarcoo</a>
+        </strong>
+        .
+      </p>
+      <p className="text-md text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,27 @@
+import { FaHome, FaExclamationCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-7xl font-bold mb-8">
+            Oops... <br />
+            <span className="text-5xl font-bold mt-3 mr-3 inline">
+              404 Error
+            </span>
+            <FaExclamationCircle className="text-5xl text-red-500 inline" />
+          </h1>
+          <p className="text-4xl mb-8">Page Not Found!</p>
+          <Link to="/" className="btn btn-primary btn-lg">
+            <FaHome className="mr-2" />
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
