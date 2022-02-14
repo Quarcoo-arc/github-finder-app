ca0806d9006703cbe6758f9baaf1831abd546342
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-14 11:45:02 +0000

    Navbar component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <NavBar />
+        <main>Content</main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+import { render } from "react-dom";
+import "./index.css";
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
