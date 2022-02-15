ae6ff2c3e30ab791f808f341d735ee73b55e255c
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-15 14:58:34 +0000

    added userItem component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,11 +1,17 @@
 import UsersContext from "../../context/UsersContext";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 import { useContext } from "react";
 
 const UserResults = () => {
   const { data, isLoading } = useContext(UsersContext);
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {!isLoading && data.map((user) => <h3 key={user.id}>{user.login}</h3>)}
+      {data.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
     </div>
   );
 };
