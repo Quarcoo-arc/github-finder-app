7632335fda0f000d8228b09f547b34661027e668
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-16 16:18:57 +0000

    Changed back button

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -40,7 +40,7 @@ const User = () => {
       <div className="w-full mx-auto lg:w-10/12">
         <div className="mb-4">
           <Link to="/" className="btn btn-ghost">
-            Back To Search
+            {"<"} Back
           </Link>
         </div>
 
